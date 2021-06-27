473e5b81ba9f5bdfdf900f143c58214e39613bf0
Paul Thomas <paulthomas2107@gmail.com>
2021-06-27 16:57:45 +0100

    Finish

--- app.js
+++ app.js
@@ -2,12 +2,33 @@
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
+const filterOption = document.querySelector(".filter-todo");
 
 // Event Listeners
 todoButton.addEventListener("click", addTodo);
 todoList.addEventListener("click", deleteCheck);
+filterOption.addEventListener("click", filterTodo);
 
 // Functions
+
+function filterTodo(event) {
+  console.log("here...");
+  const todos = todoList.childNodes;
+  todos.forEach(function (todo) {
+    switch (event.target.value) {
+      case "all":
+        todo.style.display = "flex";
+        break;
+      case "completed":
+        if (todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+    }
+  });
+}
+
 function addTodo(event) {
   event.preventDefault();
   // Todo Div
