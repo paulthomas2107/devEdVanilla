a40cb7ff58bfe7080973b5de9466deabc203f249
Paul Thomas <paulthomas2107@gmail.com>
2021-06-27 15:04:16 +0100

    Predelete

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ function addTodo(event) {
   todoDiv.classList.add("todo");
   // Create LI
   const newTodo = document.createElement("li");
-  newTodo.innerText = "Hey";
-  newTodo.classList.add("todo-item");   
+  newTodo.innerText = todoInput.value;
+  newTodo.classList.add("todo-item");
   todoDiv.appendChild(newTodo);
   // Check Mark Button
   const completedButton = document.createElement("button");
@@ -25,8 +25,10 @@ function addTodo(event) {
   // Check Trash Button
   const trashButton = document.createElement("button");
   trashButton.innerHTML = '<i class="fas fa-trash"></i>';
-  trashButton.classList.add("complete-btn");
+  trashButton.classList.add("trash-btn");
   todoDiv.appendChild(trashButton);
   // Append to List
   todoList.appendChild(todoDiv);
+  // Clear todo
+  todoInput.value = "";
 }
